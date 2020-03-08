fb12040d9c2dd623036ab183484fba41ed524762
Julia Wang <juliawang105@gmail.com>
2020-03-07 18:01:05 -0800

    fix webpack

---
+++ frontend/scooch.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('DOMContentLoaded', () => {
+  debugger
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>It is Alive</h1>, root)
+});
